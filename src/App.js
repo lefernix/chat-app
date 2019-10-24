f370bd4b362e395f9dc93c7d4e4828e4d8c4d322
Nombalier <a.nombalier@gmail.com>
2019-10-24 16:48:30 +0200

    add stat instead of function

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Contact from './components/Contact.js';
 function App() {
   return (
     <div className="App">
-      <Contact name="Bob Harcker" avatarURL="https://randomuser.me/api/portraits/men/56.jpg" online/>
+      <Contact name="Bob Harcker" avatarURL="https://randomuser.me/api/portraits/men/56.jpg"/>
       <Contact name="Thierry Durant" avatarURL="https://randomuser.me/api/portraits/men/17.jpg" />
-      <Contact name="Ashley Frey" avatarURL="https://randomuser.me/api/portraits/women/65.jpg" online/>
+      <Contact name="Ashley Frey" avatarURL="https://randomuser.me/api/portraits/women/65.jpg" />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import './Contact.css';
 
-
-
-function Contact({ name, avatarURL, online }) {
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.online
+    };
+  }
+  
+  render(){
     return (
       <div className="Contact">
-        <img className="avatar" src={avatarURL} ></img>
+        <img className="avatar" src={this.props.avatarURL} ></img>
         <div>
-          <h3 className="name">{name}</h3>
-          <div className="status">
-            <span className={ online ? "status-online" : "status-offline"}></span>
-            <span className="status-text">{ online ? "Online" : "Offline"}</span>
+          <h3 className="name">{this.props.name}</h3>
+          <div className="status" onClick={() =>{
+            const changeStatus = !this.state.online;
+            this.setState({online : changeStatus});
+          }}>
+            <span className={ this.state.online ? "status-online" : "status-offline"}></span>
+            <span className="status-text">{ this.state.online ? "Online" : "Offline"}</span>
           </div>
          </div>
       </div>
     );
   }
+}
+    
 
 export default Contact;
